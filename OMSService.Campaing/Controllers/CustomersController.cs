aa19beed8cf9e07fc678e3dbdddc7a8d934590f6
Araujo <Ovidio.Araujo@unisys.com>
2018-10-05 12:18:26 -0500

    Primer commit service Rest

---
+++ OMSService.Campaing/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Web.Http;
+
+namespace OMSService.Campaing.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+        public IHttpActionResult GetId(int id)
+        {
+            var customerFake = "customer-fake";
+            return Ok(customerFake);
+        }
+
+        public IHttpActionResult GetAll()
+        {
+            var customersFake = new string[] { "customer-1", "customer-2", "customer-3" };
+            return Ok(customersFake);
+        }
+    }
+}
